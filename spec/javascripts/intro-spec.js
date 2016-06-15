3e672b23d9594a269163cfecf6b6fe19089b9528
Nathaniel Miller <dukeoflaser@gmail.com>
2016-06-15 22:18:29 +0000

    Done.

---
+++ spec/javascripts/intro-spec.js
@@ -0,0 +1,32 @@
+'use strict';
+describe('Intro to Jasmine and JS basics', function() {
+  
+  describe('#favIceCream', function() {
+    it('should return your fav icecream', function() {
+      expect(favIceCream("mint chocolate chip")).toBe("I love mint chocolate chip");
+    });
+    
+  });
+
+  describe('#shouting', function() {
+    it('should return a string in all caps', function() {
+    
+      expect(shouting("i love javascript soooooo much")).toBe('I LOVE JAVASCRIPT SOOOOOO MUCH');
+    });
+  });
+
+  describe('#roundDown', function() {
+    it('should correctly floor the number', function() {
+      expect(roundDown(5.99)).toBe(5);
+    });
+  });
+
+  describe('#theTruth', function() {
+    it('return true', function() {
+      expect(theTruth()).toBe(true);
+    });
+    
+  });
+
+ 
+});
